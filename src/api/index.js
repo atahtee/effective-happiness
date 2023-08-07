9cd840c90febd85a99b1dc424f9ef838f8bd031c
atahtee <atatisam14@gmail.com>
2023-08-07 21:11:20 +0300

    Changed the backend url

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //const url = 'http://:5000/posts'; focus time
 //const url = 'https://frame-it-djm7.onrender.com/';
-const url ='https://frame-it-backend.onrender.com';
+const url ='https://frame-it-backend.onrender.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url,newPost);
@@ -10,4 +10,3 @@ export const updatePost = (id, updatePost) => axios.patch(`${url}/${id}`, update
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
 
-//yy
